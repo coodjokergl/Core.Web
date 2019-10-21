45c2ceb2973b238ae038fd5d9c84a55a8a614089
龚磊 <Gongl01@mingyuanyun.com>
2019-10-21 12:47:23 +0800

    修改:
    Signed-off-by: 龚磊 <Gongl01@mingyuanyun.com>

---
+++ Core.Web/Core/CoreActionInvoker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Core.Web.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CoreActionInvoker :IActionInvoker
+    {
+        public Task InvokeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
